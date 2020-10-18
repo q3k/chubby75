c189ff1bfd42d98a9754cbbc3757f3e1c828c495
Your Name <you@example.com>
2020-10-18 17:29:45 +0200

    Add support for scanning CABGA256 package

--- 5a-75b/pin-scan/gen_lpf.py
+++ 5a-75b/pin-scan/gen_lpf.py
@@ -2,31 +2,46 @@
 
 import binascii
 import csv
-import sys
+import argparse
 
 blacklist = [
-	'PROGRAMN',
+    'PROGRAMN',
 ]
 
-cr = csv.reader(open(sys.argv[1], 'r'))
+PACKAGES = {
+        'CABGA381': 7,
+        'CABGA285': 8,
+        'CABGA256': 9
+}
 
-with open(sys.argv[2], 'w') as lpf, open(sys.argv[3], 'wb') as rom:
-	i = 0
-	for l in cr:
-		if not l[0].isdigit():
-			continue
-		if not l[7].isalnum():
-			continue
-		if l[1][0:2] not in ('PT', 'PB', 'PL', 'PR'):
-			continue
-		if l[1] in blacklist:
-			continue
+parser = argparse.ArgumentParser()
+parser.add_argument("pinout", help="The pinout file")
+parser.add_argument("lpf", help="The output LPF file")
+parser.add_argument("rom", help="The output .hex file")
+parser.add_argument("--package", help="The target package",
+                    choices=PACKAGES.keys())
 
-		name = l[1]
-		pad  = l[7]
+args = parser.parse_args()
 
-		lpf.write('LOCATE COMP "pads[%d]" SITE "%s";\t# %s\n' % (i, pad, name))
-		lpf.write('IOBUF PORT "pads[%d]" PULLMODE=UP;\n' % (i,))
-		rom.write((binascii.b2a_hex(pad.encode('utf-8')) + b'00')[0:6] + b'\n')
+cr = csv.reader(open(args.pinout, 'r'))
 
-		i=i+1
+with open(args.lpf, 'w') as lpf, open(args.rom, 'wb') as rom:
+    i = 0
+    for l in cr:
+        if not l[0].isdigit():
+            continue
+        if not l[PACKAGES[args.package]].isalnum():
+            continue
+        if l[1][0:2] not in ('PT', 'PB', 'PL', 'PR'):
+            continue
+        if l[1] in blacklist:
+            continue
+
+        name = l[1]
+        pad  = l[PACKAGES[args.package]]
+
+        lpf.write('LOCATE COMP "pads[%d]" SITE "%s";\t# %s\n' % (i, pad, name))
+        lpf.write('IOBUF PORT "pads[%d]" PULLMODE=UP;\n' % (i,))
+        rom.write((binascii.b2a_hex(pad.encode('utf-8')) + b'00')[0:6] + b'\n')
+
+        i = i+1
